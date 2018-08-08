4990fcfb50051662485380cdeb73ff7b6e94a89b
Eric Jacobson <contact@efjacobson.com>
2018-08-07 19:11:26 -0700

    compiled

---
+++ build/php/src/Tmz/Schemas/Curator/Command/SyncTeaser.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://schemas.tmz.com/json-schema/tmz/curator/command/sync-teaser/latest.json#
+namespace Tmz\Schemas\Curator\Command;
+
+use Gdbots\Pbj\Message;
+
+interface SyncTeaser extends Message
+{
+}
