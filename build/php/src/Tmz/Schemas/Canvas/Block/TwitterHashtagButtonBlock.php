a7070d5e623cc1c7fec82097f1a2be2b408e903e
Arun Karnati <arun.karnati@gmail.com>
2018-10-15 18:20:49 -0700

    delete pinterest, twitter schemas

--- build/php/src/Tmz/Schemas/Canvas/Block/TwitterHashtagButtonBlock.php
+++
@@ -1,9 +0,0 @@
-<?php
-// @link http://schemas.tmz.com/json-schema/tmz/canvas/block/twitter-hashtag-button-block/latest.json#
-namespace Tmz\Schemas\Canvas\Block;
-
-use Gdbots\Pbj\Message;
-
-interface TwitterHashtagButtonBlock extends Message
-{
-}
--- build/php/src/Tmz/Schemas/Canvas/Block/TwitterSearchTimelineBlock.php
+++
@@ -1,9 +0,0 @@
-<?php
-// @link http://schemas.tmz.com/json-schema/tmz/canvas/block/twitter-search-timeline-block/latest.json#
-namespace Tmz\Schemas\Canvas\Block;
-
-use Gdbots\Pbj\Message;
-
-interface TwitterSearchTimelineBlock extends Message
-{
-}
