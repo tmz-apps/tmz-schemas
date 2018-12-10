1af83c23666daf6ba69a16675e5703b6d4a38c6a
Greg Brown <greg@12dnetworks.com>
2018-12-09 22:06:34 -0800

    v0.2.0 with render widget and promotion and use triniti/schemas v0.9.0. (#7)
    * Use triniti/schemas v0.9.0.
    
    * 0.2.0
    
    * restore build-site script

---
+++ build/php/src/Tmz/Schemas/Curator/Request/RenderPromotionResponse.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://schemas.tmz.com/json-schema/tmz/curator/request/render-promotion-response/latest.json#
+namespace Tmz\Schemas\Curator\Request;
+
+use Gdbots\Pbj\Message;
+
+interface RenderPromotionResponse extends Message
+{
+}
---
+++ build/php/src/Tmz/Schemas/Curator/Request/RenderWidgetResponse.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://schemas.tmz.com/json-schema/tmz/curator/request/render-widget-response/latest.json#
+namespace Tmz\Schemas\Curator\Request;
+
+use Gdbots\Pbj\Message;
+
+interface RenderWidgetResponse extends Message
+{
+}
