08e04499b76308962054da4aeeefbbe1906823fb
Eric Jacobson <contact@efjacobson.com>
2019-10-31 15:11:33 -0700

    use triniti/schemas 1.1.2, add new blocks

---
+++ build/php/src/Tmz/Schemas/Canvas/Block/SpotifyEmbedBlock.php
@@ -0,0 +1,9 @@
+<?php
+// @link https://schemas.tmz.com/json-schema/tmz/canvas/block/spotify-embed-block/latest.json#
+namespace Tmz\Schemas\Canvas\Block;
+
+use Gdbots\Pbj\Message;
+
+interface SpotifyEmbedBlock extends Message
+{
+}
---
+++ build/php/src/Tmz/Schemas/Canvas/Block/TiktokEmbedBlock.php
@@ -0,0 +1,9 @@
+<?php
+// @link https://schemas.tmz.com/json-schema/tmz/canvas/block/tiktok-embed-block/latest.json#
+namespace Tmz\Schemas\Canvas\Block;
+
+use Gdbots\Pbj\Message;
+
+interface TiktokEmbedBlock extends Message
+{
+}
